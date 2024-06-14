721b079408101a9889c3a6b96dc3dcf34f0cf26d
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-13 23:28:55 -0300

    Add Apollo GraphQL server and Lesson module with resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver((_of) => LessonType)
+export class LessonResolver {
+  @Query((_returns) => LessonType)
+  lesson(): LessonType {
+    return {
+      id: '',
+      name: '',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
